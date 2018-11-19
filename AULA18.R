fc76ec7555610247abee97ad7ab9f905bb45fdc4
regianeflavia27 <42551128+regianeflavia27@users.noreply.github.com>
2018-11-19 11:25:53 -0200

    Add files via upload

---
+++ AULA18.R
@@ -0,0 +1,314 @@
+> pacotes <- c("quantmod","Quandl","forecast","dplyr","magritt",
+               + "highcharter","dygraphs","htmltools","ggplot2","MTS",
+               + "vars","urca","seasonal")
+> install.packages(pacotes)
+Installing packages into 'C:/Users/Alunos/Documents/R/win-library/3.5'
+(as 'lib' is unspecified)
+Warning in install.packages :
+  package 'magritt' is not available (for R version 3.5.1)
+also installing the dependencies 'XML', 'data.table', 'tidyselect', 'timeSeries', 'fastICA', 'gss', 'stabledist', 'htmlwidgets', 'rlist', 'broom', 'tidyr', 'igraph', 'lubridate', 'fGarch', 'fBasics', 'mvtnorm', 'x13binary'
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/XML_3.98-1.16.zip'
+Content type 'application/zip' length 4602444 bytes (4.4 MB)
+downloaded 4.4 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/data.table_1.11.8.zip'
+Content type 'application/zip' length 1831955 bytes (1.7 MB)
+downloaded 1.7 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/tidyselect_0.2.5.zip'
+Content type 'application/zip' length 623252 bytes (608 KB)
+downloaded 608 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/timeSeries_3042.102.zip'
+Content type 'application/zip' length 1829028 bytes (1.7 MB)
+downloaded 1.7 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/fastICA_1.2-1.zip'
+Content type 'application/zip' length 61674 bytes (60 KB)
+downloaded 60 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/gss_2.1-9.zip'
+Content type 'application/zip' length 1540858 bytes (1.5 MB)
+downloaded 1.5 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/stabledist_0.7-1.zip'
+Content type 'application/zip' length 75312 bytes (73 KB)
+downloaded 73 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/htmlwidgets_1.3.zip'
+Content type 'application/zip' length 805051 bytes (786 KB)
+downloaded 786 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/rlist_0.4.6.1.zip'
+Content type 'application/zip' length 231904 bytes (226 KB)
+downloaded 226 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/broom_0.5.0.zip'
+Content type 'application/zip' length 2010859 bytes (1.9 MB)
+downloaded 1.9 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/tidyr_0.8.2.zip'
+Content type 'application/zip' length 947951 bytes (925 KB)
+downloaded 925 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/igraph_1.2.2.zip'
+Content type 'application/zip' length 9147896 bytes (8.7 MB)
+downloaded 8.7 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/lubridate_1.7.4.zip'
+Content type 'application/zip' length 1568424 bytes (1.5 MB)
+downloaded 1.5 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/fGarch_3042.83.zip'
+Content type 'application/zip' length 605905 bytes (591 KB)
+downloaded 591 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/fBasics_3042.89.zip'
+Content type 'application/zip' length 2668827 bytes (2.5 MB)
+downloaded 2.5 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/mvtnorm_1.0-8.zip'
+Content type 'application/zip' length 270167 bytes (263 KB)
+downloaded 263 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/x13binary_1.1.39-1.zip'
+Content type 'application/zip' length 1178880 bytes (1.1 MB)
+downloaded 1.1 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/quantmod_0.4-13.zip'
+Content type 'application/zip' length 943204 bytes (921 KB)
+downloaded 921 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/Quandl_2.9.1.zip'
+Content type 'application/zip' length 54743 bytes (53 KB)
+downloaded 53 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/forecast_8.4.zip'
+Content type 'application/zip' length 1997548 bytes (1.9 MB)
+downloaded 1.9 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/dplyr_0.7.7.zip'
+Content type 'application/zip' length 3071372 bytes (2.9 MB)
+downloaded 2.9 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/highcharter_0.5.0.zip'
+Content type 'application/zip' length 4591137 bytes (4.4 MB)
+downloaded 4.4 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/dygraphs_1.1.1.6.zip'
+Content type 'application/zip' length 441358 bytes (431 KB)
+downloaded 431 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/htmltools_0.3.6.zip'
+Content type 'application/zip' length 660736 bytes (645 KB)
+downloaded 645 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/ggplot2_3.1.0.zip'
+Content type 'application/zip' length 3622882 bytes (3.5 MB)
+downloaded 3.5 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/MTS_1.0.zip'
+Content type 'application/zip' length 1403090 bytes (1.3 MB)
+downloaded 1.3 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/vars_1.5-3.zip'
+Content type 'application/zip' length 964097 bytes (941 KB)
+downloaded 941 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/urca_1.3-0.zip'
+Content type 'application/zip' length 1060687 bytes (1.0 MB)
+downloaded 1.0 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/seasonal_1.6.1.zip'
+Content type 'application/zip' length 480422 bytes (469 KB)
+downloaded 469 KB
+
+package 'XML' successfully unpacked and MD5 sums checked
+package 'data.table' successfully unpacked and MD5 sums checked
+package 'tidyselect' successfully unpacked and MD5 sums checked
+package 'timeSeries' successfully unpacked and MD5 sums checked
+package 'fastICA' successfully unpacked and MD5 sums checked
+package 'gss' successfully unpacked and MD5 sums checked
+package 'stabledist' successfully unpacked and MD5 sums checked
+package 'htmlwidgets' successfully unpacked and MD5 sums checked
+package 'rlist' successfully unpacked and MD5 sums checked
+package 'broom' successfully unpacked and MD5 sums checked
+package 'tidyr' successfully unpacked and MD5 sums checked
+package 'igraph' successfully unpacked and MD5 sums checked
+package 'lubridate' successfully unpacked and MD5 sums checked
+package 'fGarch' successfully unpacked and MD5 sums checked
+package 'fBasics' successfully unpacked and MD5 sums checked
+package 'mvtnorm' successfully unpacked and MD5 sums checked
+package 'x13binary' successfully unpacked and MD5 sums checked
+package 'quantmod' successfully unpacked and MD5 sums checked
+package 'Quandl' successfully unpacked and MD5 sums checked
+package 'forecast' successfully unpacked and MD5 sums checked
+package 'dplyr' successfully unpacked and MD5 sums checked
+package 'highcharter' successfully unpacked and MD5 sums checked
+package 'dygraphs' successfully unpacked and MD5 sums checked
+package 'htmltools' successfully unpacked and MD5 sums checked
+package 'ggplot2' successfully unpacked and MD5 sums checked
+package 'MTS' successfully unpacked and MD5 sums checked
+package 'vars' successfully unpacked and MD5 sums checked
+package 'urca' successfully unpacked and MD5 sums checked
+package 'seasonal' successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\Alunos\AppData\Local\Temp\RtmpEp6FvA\downloaded_packages
+> suppressMessages(require(quantmod))
+> suppressMessages(require(Quandl))
+> suppressMessages(require(forecast))
+> suppressMessages(require(dplyr))
+> suppressMessages(require(highcharter))
+> suppressMessages(require(dygraphs))
+> suppressMessages(require(htmltools))
+> suppressMessages(require(ggplot2))
+> suppressMessages(require(MTS))
+> suppressMessages(require(vars))
+> suppressMessages(require(urca))
+> suppressMessages(require(seasonal))
+> suppressMessages(require(pwt8))
+> data("pwt8.0")
+> View(pwt8.0)
+> br <- subset(pwt8.0, country=="Brazil",
+               + select = c("rgdpna","emp","xr"))
+> colnames(br) <-  c("PIB","Emprego","Câmbio")
+> BR <- br[45:62,1:3]
+> PIB <- diff(br$PIB)
+> PIb <- ts(PIB, start = 1950, frequency = 1)
+> EMPREGO <- diff(br$Emprego)
+> Emprego <- ts(EMPREGO, start = 1950, frequency = 1)
+> CAMBIO <- diff(br$Câmbio)
+Error: unexpected input in "CAMBIO <- diff(br$Câ"
+> CAMBIO <- diff(br$Cambio)
+> Cambio <- ts(CAMBIO, start = 1950, frequency = 1)
+Error in ts(CAMBIO, start = 1950, frequency = 1) : 
+  'ts' object must have one or more observations
+> colnames(br) <-  c("PIB","Emprego","Cambio")   #Renomeia as colunas para PIB, Trabalho e Câmbio
+> BR <- br[45:62,1:3]
+> 
+  > 
+  > #Separando as variáveis
+  > PIB <- diff(br$PIB)                    #Cria o vetor para variável PIB 
+> PIb <- ts(PIB, start = 1950, frequency = 1)
+> EMPREGO <- diff(br$Emprego)            #Cria o vetor para variável EMPREGO
+> Emprego <- ts(EMPREGO, start = 1950, frequency = 1)
+> CAMBIO <- diff(br$Cambio)              #Cria o vetor para variável CAMBIO
+> Cambio <- ts(CAMBIO, start = 1950, frequency = 1)
+> Brasil <- cbind(PIB,EMPREGO,CAMBIO)
+> Anos <- seq(from=1950, to=2011, by=1)         #Cria um vetor para o tempo em anos de 1994 até 2011
+> BRA <- ts(Brasil, start = 1950, frequency = 1)
+> plot(BRA,main="Variação do PIB, Emprego e Cambio no Brasil", col="Blue")
+> h1 = hchart(PIb, name = "PIB", color = "#B71C1C")
+> hc_title(h1,text = "Variacao do PIB do Brasil", margin = 10, style = list(fontSize= "14px")) #Gráfico do PIB
+> 
+  > h2 = hchart(Emprego, name = "EMPREGO", color = "black")
+>   hc_title(h2,text = "Variacao do Emprego", margin = 10, style = list(fontSize= "14px")) #Gráfico do Emprego
+> 
+  > h3 = hchart(Cambio, name = "Cambio", color = "green")
+>   hc_title(h3,text = "Variacao do Cambio (R$/U$$)", margin = 10, style = list(fontSize= "14px")) #Gráfico do Câmbio
+> 
+  > lst = list(h1,h2,h3)
+> hw_grid(lst, ncol = 2, rowheight = 400)  %>% browsable()  #Todos Três Gráficos Juntos
+> CriterioInformacao = vars::VARselect(y = Brasil, lag.max = 6, type = "const")
+> print(CriterioInformacao$criteria)
+1            2            3            4            5            6
+AIC(n) 1.776620e+01 1.789226e+01 1.801891e+01 1.819666e+01 1.804561e+01 1.820731e+01
+HQ(n)  1.793556e+01 1.818864e+01 1.844232e+01 1.874709e+01 1.872306e+01 1.901178e+01
+SC(n)  1.820416e+01 1.865869e+01 1.911382e+01 1.962004e+01 1.979746e+01 2.028763e+01
+FPE(n) 5.201132e+07 5.919931e+07 6.773918e+07 8.214438e+07 7.238479e+07 8.829698e+07
+> 
+  > #Estimando um Var
+  > 
+  > modelobra = vars::VAR(y = Brasil, p = 1, type = "const")
+> summary(modelobra)
+
+VAR Estimation Results:
+  ========================= 
+  Endogenous variables: PIB, EMPREGO, CAMBIO 
+Deterministic variables: const 
+Sample size: 60 
+Log Likelihood: -768.821 
+Roots of the characteristic polynomial:
+  0.4177 0.2463 0.1125
+Call:
+  vars::VAR(y = Brasil, p = 1, type = "const")
+
+
+Estimation results for equation PIB: 
+  ==================================== 
+  PIB = PIB.l1 + EMPREGO.l1 + CAMBIO.l1 + const 
+
+Estimate Std. Error t value Pr(>|t|)   
+PIB.l1     2.277e-01  1.413e-01   1.611  0.11280   
+EMPREGO.l1 8.243e+02  3.017e+03   0.273  0.78570   
+CAMBIO.l1  6.057e+03  2.101e+04   0.288  0.77416   
+const      2.029e+04  5.913e+03   3.432  0.00113 **
+  ---
+  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
+
+Residual standard error: 28580 on 56 degrees of freedom
+Multiple R-Squared: 0.0571,	Adjusted R-squared: 0.006591 
+F-statistic:  1.13 on 3 and 56 DF,  p-value: 0.3446 
+
+
+Estimation results for equation EMPREGO: 
+  ======================================== 
+  EMPREGO = PIB.l1 + EMPREGO.l1 + CAMBIO.l1 + const 
+
+Estimate Std. Error t value Pr(>|t|)    
+PIB.l1     3.994e-06  6.625e-06   0.603 0.549062    
+EMPREGO.l1 1.130e-01  1.414e-01   0.799 0.427794    
+CAMBIO.l1  9.913e-01  9.848e-01   1.007 0.318430    
+const      1.047e+00  2.772e-01   3.778 0.000385 ***
+  ---
+  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
+
+Residual standard error: 1.34 on 56 degrees of freedom
+Multiple R-Squared: 0.03592,	Adjusted R-squared: -0.01572 
+F-statistic: 0.6955 on 3 and 56 DF,  p-value: 0.5587 
+
+
+Estimation results for equation CAMBIO: 
+  ======================================= 
+  CAMBIO = PIB.l1 + EMPREGO.l1 + CAMBIO.l1 + const 
+
+Estimate Std. Error t value Pr(>|t|)    
+PIB.l1      3.230e-07  8.331e-07   0.388 0.699724    
+EMPREGO.l1 -8.729e-03  1.779e-02  -0.491 0.625523    
+CAMBIO.l1   4.359e-01  1.238e-01   3.520 0.000866 ***
+  const       1.781e-02  3.486e-02   0.511 0.611526    
+---
+  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
+
+Residual standard error: 0.1685 on 56 degrees of freedom
+Multiple R-Squared: 0.1923,	Adjusted R-squared: 0.149 
+F-statistic: 4.444 on 3 and 56 DF,  p-value: 0.007172 
+
+
+
+Covariance matrix of residuals:
+  PIB    EMPREGO     CAMBIO
+PIB     816638891  1.341e+04 -1.222e+03
+EMPREGO     13414  1.795e+00 -5.232e-02
+CAMBIO      -1222 -5.232e-02  2.838e-02
+
+Correlation matrix of residuals:
+  PIB EMPREGO  CAMBIO
+PIB      1.0000  0.3504 -0.2539
+EMPREGO  0.3504  1.0000 -0.2318
+CAMBIO  -0.2539 -0.2318  1.0000
+
+
+> CriterioInformacao = vars::VARselect(y = Brasil, lag.max = 6, type = "const")
+> print(CriterioInformacao$criteria)
+1            2            3            4            5            6
+AIC(n) 1.776620e+01 1.789226e+01 1.801891e+01 1.819666e+01 1.804561e+01 1.820731e+01
+HQ(n)  1.793556e+01 1.818864e+01 1.844232e+01 1.874709e+01 1.872306e+01 1.901178e+01
+SC(n)  1.820416e+01 1.865869e+01 1.911382e+01 1.962004e+01 1.979746e+01 2.028763e+01
+FPE(n) 5.201132e+07 5.919931e+07 6.773918e+07 8.214438e+07 7.238479e+07 8.829698e+07
+> 
